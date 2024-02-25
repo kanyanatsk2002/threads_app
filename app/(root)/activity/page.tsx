cc1dfdf7fc3203c7f61af9c2acfc54f330ec2bf5
kanyanatsk2002 <kanyanat.sk.2002@gmail.com>
2024-02-25 14:10:21 +0700

    version1 redirect activity

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,17 +1,16 @@
 import Image from "next/image";
 import Link from "next/link";
 import { currentUser } from "@clerk/nextjs";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 import { fetchUser, getActivity } from "@/lib/actions/user.actions";
 
 async function Page() {
-  const router = useRouter();
   const user = await currentUser();
   if (!user) return null;
 
   const userInfo = await fetchUser(user.id);
-  if (!userInfo?.onboarded) router.push("/onboarding");
+  if (!userInfo?.onboarded) redirect("/onboarding");
 
   const activity = await getActivity(userInfo._id);
 
