821b9a93e05d5f3d5436bd3673cad7049c30f5b3
kanyanatsk2002 <kanyanat.sk.2002@gmail.com>
2024-03-01 10:09:37 +0700

    fix dark theme

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -20,9 +20,9 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider
-      appearance={{
-        baseTheme: dark,
-      }}
+      // appearance={{
+      //   baseTheme: dark,
+      // }}
     >
       <html lang='en'>
         <body className={`${inter.className} bg-dark-1`}>
