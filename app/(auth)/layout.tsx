112f086c3fbe41b2c20d80c9ecd6597ad503da82
kanyanatsk2002 <kanyanat.sk.2002@gmail.com>
2024-03-01 10:13:50 +0700

    fix dark theme

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -19,10 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider
-      appearance={{
-        baseTheme: dark,
-      }}
+    <ClerkProvider  
     >
       <html lang="en">
         <body className={`${inter.className}`}>
